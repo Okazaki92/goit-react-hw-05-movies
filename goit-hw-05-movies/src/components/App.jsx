6d0eb07bc94198d28b84df682e92607d43b92759
Okazaki92 <vincentslominski@gmail.com>
2023-06-23 12:02:07 +0200

    feat/add suspense, set api_key and language as default

--- goit-hw-05-movies/src/components/App.jsx
+++ goit-hw-05-movies/src/components/App.jsx
@@ -5,19 +5,23 @@ import { MoviesSearch } from "../pages/MoviesSearch/MoviesSearch";
 import { MovieDetails } from "../pages/MovieDetails/MovieDetails";
 import { Cast } from "./Cast/Cast";
 import { Reviews } from "./Reviews/Reviews";
+import { Suspense } from "react";
+import { Loader } from "./Loader/Loader";
 
 export const App = () => {
   return (
     <div className="container">
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<MoviesSearch />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<MoviesSearch />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- goit-hw-05-movies/src/services/movies-api.js
+++ goit-hw-05-movies/src/services/movies-api.js
@@ -1,38 +1,35 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3/";
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.params = {};
+axios.defaults.params.api_key = API_KEY;
+axios.defaults.params.language = "en-US";
+
 export const getTrendingMovies = async () => {
-  const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+  const response = await axios.get(`trending/movie/day?`);
   return response.data.results;
 };
 
 export const getQueryMovies = async (query) => {
   const response = await axios.get(
-    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+    `search/movie?page=1&include_adult=false&query=${query}`
   );
   return response.data.results;
 };
 
 export const getMovieDetails = async (movieId) => {
-  const response = await axios.get(
-    `movie/${movieId}?api_key=${API_KEY}&language=en-US`
-  );
+  const response = await axios.get(`movie/${movieId}?`);
   return response.data;
 };
 
 export const getMovieCredits = async (movieId) => {
-  const response = await axios.get(
-    `movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
-  );
+  const response = await axios.get(`movie/${movieId}/credits?`);
   return response.data.cast;
 };
 
 export const getMovieReviews = async (movieId) => {
-  const response = await axios.get(
-    `movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
-  );
+  const response = await axios.get(`movie/${movieId}/reviews?page=1`);
   return response.data.results;
 };
